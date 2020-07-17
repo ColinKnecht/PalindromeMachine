817e43f3c2bc6c4deab9a6df063d5482f0551d6a
ColinKnecht <cknecht87@gmail.com>
2020-07-17 11:12:38 -0400

    adding console colors + finishing functionality

--- Program.cs
+++ Program.cs
@@ -6,28 +6,38 @@ using System.Text.RegularExpressions;
 
 namespace ColinKnecht_PalindromeMachine
 {
+
     class Program
     {
-        //Using VS Code, write a C# solution to take in an input of a paragraph and:
-        //Give the number of palindrome words
-        //Give the number of palindrome sentences
-        //List the unique words of a paragraph with the count of the word instance
-        //Let the user also input a letter at some point and list all words containing that letter
-        //Write up a short documentation explaining what your program does.
+        //Colin Knecht
+        //cknecht87@gmail.com
+        //740-817-1006
+
+        //****Write up a short documentation explaining what your program does.
+        //My program takes in a user inputted paragraph; which in turn.....
+        //counts the number of palindrome words
+        //counts the number of palindrome sentences
+        //Lists all unique words with the total number of times a count is used
+        //accepts a user inputted letter to check how many words have letter user entered
+        private const string CHARACTERS_TO_REPLACE = "[*'\",:;().-?!_&#^@]+";
         static void Main(string[] args)
         {
-            //TODO's - get rid of 1 letter palindromes
-            //check for copy paste multiple lines
-            // get ride of somewhere? in last example when entering 'o'
+            TurnConsoleColorBlue();
             Console.WriteLine("Welcome to the Palindrome Machine!  Please enter your paragraph to check....");
             string paragraph = Console.ReadLine();
+
+            TurnConsoleColorRed();
             Console.WriteLine("Number of palindrome words in paragraph is: " + CountPalindromeWords(paragraph).ToString());
             Console.WriteLine("Number of palindrome sentences in paragraph is: " + CountPalindromeSentences(paragraph).ToString());
-            ListUniqueWordsWithCount(paragraph);
+
+            TurnConsoleColorGreen();
+            PrintUniqueWordsToConsole(ListUniqueWordsWithCount(paragraph));
+            
+            TurnConsoleColorYellow();
             Console.WriteLine("=================================================");
             Console.WriteLine("Please enter a letter to check which words have that letter.....");
             string letter = Console.ReadLine();
-            
+
             while(letter.Length != 1)
             {
                 Console.WriteLine("Please enter a 1 letter character!! Enter letter below...");
@@ -35,21 +45,23 @@ namespace ColinKnecht_PalindromeMachine
             }
             CheckUserInputLetter(letter, paragraph);
 
+            TurnConsoleColorWhite();
             Console.WriteLine("Thanks for playing!! Enter any key to exit!!");
             Console.ReadLine();
         }
 
-        public static int CountPalindromeWords(string paragraph)
+        private static int CountPalindromeWords(string paragraph) //Give the number of palindrome words
         {
             int palindromeCount = 0;
             //remove periods and special characters
-            paragraph = Regex.Replace(paragraph, "[';,().-]+", "");
+            paragraph = Regex.Replace(paragraph, CHARACTERS_TO_REPLACE, "");
             //turn paragraph into array of words
             string[] words = paragraph.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
 
-            foreach(string word in words)
+            foreach(string wrd in words)
             {
-                if (word.SequenceEqual(word.Reverse()))
+                string word = wrd.ToLower();
+                if (word.SequenceEqual(word.Reverse()) && word.Length > 1)
                 {
                     palindromeCount++;
                 }
@@ -58,20 +70,20 @@ namespace ColinKnecht_PalindromeMachine
             return palindromeCount;
         }
 
-        public static int CountPalindromeSentences(string paragraph)
+        private static int CountPalindromeSentences(string paragraph) //Give the number of palindrome sentences
         {
             int palindromeCount = 0;
             //split paragraph into array of sentences
             char[] separators = new char[] { '!', '.', '?' };
             string[] sentences = paragraph.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-            
+
             foreach(string sentence in sentences)
-            { 
-                string sent = Regex.Replace(sentence, "[';,().-]+", "");
+            {
+                string sent = Regex.Replace(sentence, CHARACTERS_TO_REPLACE, "");
                 //squash sentences into just text.. no special chars
                 sent = Regex.Replace(sent, @"\s", "");
                 sent = sent.ToLower(); //ignore case
-                if (sent.SequenceEqual(sent.Reverse()))
+                if (sent.SequenceEqual(sent.Reverse()) && sent.Length > 1)
                 {
                     palindromeCount++;
                 }
@@ -80,7 +92,7 @@ namespace ColinKnecht_PalindromeMachine
             return palindromeCount;
         }
 
-        public static void ListUniqueWordsWithCount(string paragraph)
+        private static Dictionary<string, int> ListUniqueWordsWithCount(string paragraph) //List the unique words of a paragraph with the count of the word instance
         {
             string[] words = paragraph.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
             string paragraphToUnique = "";
@@ -88,7 +100,7 @@ namespace ColinKnecht_PalindromeMachine
             for (int i = 0; i < words.Length; i++)
             {
                 //remove special characters from word
-                words[i] = Regex.Replace(words[i], "[';,().-]+", "");
+                words[i] = Regex.Replace(words[i], CHARACTERS_TO_REPLACE, "");
                 //strip out the apostrophes and special characters in words to add to uniqueWords hashset
                 paragraphToUnique += words[i] + " ";
             }
@@ -122,19 +134,28 @@ namespace ColinKnecht_PalindromeMachine
                     }
                 }
             }
-            
+            return wordCountDictionary;
         }
 
-        public static void CheckUserInputLetter(string letter, string paragraph)
+        private static void PrintUniqueWordsToConsole(Dictionary<string, int> wordCountDictionary) //print unique words
         {
-            //Let the user also input a letter at some point and list all words containing that letter
+            var sortedDict = from entry in wordCountDictionary orderby entry.Value descending select entry;
+
+            foreach(KeyValuePair<string, int> entry in sortedDict)
+            {
+                Console.WriteLine(entry.Key + " --------- occurs " + entry.Value + " times.");
+            }
+        }
+
+        private static void CheckUserInputLetter(string letter, string paragraph) //Let the user also input a letter at some point and list all words containing that letter
+        {
             string letterIgnoreCase = letter.ToString().ToLower();
             ArrayList wordsWithLetter = new ArrayList();
             string[] words = paragraph.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
             for (int i = 0; i < words.Length; i++)
             {
                 //remove special characters from word
-                words[i] = Regex.Replace(words[i], "[';,().-]+", "");
+                words[i] = Regex.Replace(words[i], CHARACTERS_TO_REPLACE, "");
             }
 
             //check for char and add it to list to return
@@ -146,8 +167,7 @@ namespace ColinKnecht_PalindromeMachine
                     wordsWithLetter.Add(word);
                 }
             }
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
+            
             if (wordsWithLetter.Count == 0)
             {
                 Console.WriteLine("There are no words containing the letter: " + letterIgnoreCase);
@@ -160,7 +180,42 @@ namespace ColinKnecht_PalindromeMachine
                     Console.WriteLine(word);
                 }
             }
+        }
+
+        private static void ResetConsoleColors()
+        {
             Console.ForegroundColor = ConsoleColor.Gray;
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        private static void TurnConsoleColorRed()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+
+        private static void TurnConsoleColorBlue()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void TurnConsoleColorGreen()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void TurnConsoleColorYellow()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+
+        private static void TurnConsoleColorWhite()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
         }
     }
 }
